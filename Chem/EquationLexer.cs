ff51a59920b23bd07ce11ffe12c4ebd313e8e60f
SemicolonUnexpected <m.gracey1409@gmail.com>
2024-02-12 15:15:50 +0000

    Added more lexer stuff

--- Chem/EquationLexer.cs
+++ Chem/EquationLexer.cs
@@ -1,22 +1,38 @@
+using static Chem.TokenType;
+
 namespace Chem;
 
 internal class EquationLexer {
     private readonly string _equation;
     private int _index = 0;
+    private List<Token> tokens = new();
 
     public EquationLexer(string equation) {
         _equation = equation;
     }
 
     public List<Token> Lex() {
-        List<Token> tokens = new();
 
         // Iterate over the whole equation string
         while(_index < _equation.Length) {
-            char current = Advance();
+            ScanToken();
         }
 
         return tokens;
+    }
+
+    private void ScanToken() {
+        char current = Advance();
+        switch (current) {
+            case '(':
+                tokens.Add(new Token(LeftParenthesis, current.ToString(), null));
+                break;
+            case ')':
+                tokens.Add(new Token(RightParenthesis, current.ToString(), null));
+                break;
+            default:
+                throw new EquationSyntaxException("Unexpected character", _equation, _index);
+        }
     }
 
     #region Lexer movements
--- Chem/Token.cs
+++ Chem/Token.cs
@@ -1,6 +1,13 @@
 namespace Chem;
 
 internal class Token {
-//    public TokenType Token
-    public Token() {}
+    public  TokenType Type { get; init; }
+    public string Lexeme { get; init; }
+    public object? Literal { get; init; }
+
+    public Token(TokenType type, string lexeme, object? literal) {
+        Type = type;
+        Lexeme = lexeme;
+        Literal = literal;
+    }
 }
